19dca38c2c30a59338f583bb28a5fab795b039b6
Huawei User <gariban_bir_kul@gariban_bir_kul.com>
2022-06-11 16:28:03 +0300

    hw02

---
+++ hw02/index.js
@@ -0,0 +1,33 @@
+// Blog oluşturmaya hazır mısınız? Konsol ekranında postlarımızı sıralayalım, sonrasında yeni bir post oluşturalım ve yeni post ile birlikte postlarımızı tekrar sıralayalım.
+
+const blogs = [
+  {id: 1, title: "Bringing forward the End-of-Life Date for Node.js 16", author: "Hamit"},
+  {id: 2, title: "Mathematical Aspects of Division Property", author: "Bilal"},
+  {id:3, title: "Working With Wifi", author: "Ayhan"}
+]
+
+const listBlogs = () => {
+  blogs.map(blog => {
+    console.log(blog.title);
+  })
+}
+
+const addBlog = newBlog => {
+  const promiseAddBlog = new Promise((resolve, reject) => {
+    blogs.push(newBlog);
+    resolve(blogs);
+  });
+
+  return promiseAddBlog;
+}
+
+async function showBlogs() {
+  try {
+    await addBlog({id: 4, title: "Advantages of Pinia vs Vuex", author: "Ahmet"});
+    listBlogs();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+showBlogs();
